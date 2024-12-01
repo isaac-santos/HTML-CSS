bef625676d06ceb4c5f7daed6a96b8b731964fb8
isaac-santos <isaacconceicao26@gmail.com>
2024-12-01 12:56:30 -0300

    atualizei o mq005

--- exercicios/modulo 4/Media Queries/mq005/script.js
+++ exercicios/modulo 4/Media Queries/mq005/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
+
 function clickmenu() {
     if (itens.style.display == 'block') {
         itens.style.display = 'none'
